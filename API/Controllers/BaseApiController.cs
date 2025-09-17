c4b2a2db303db1c31dce0bfbc3e3d7b31e34ad09
kasja <kasjanlaboj@gmail.com>
2025-09-17 18:06:39 +0200

    add base controller

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Core;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+        protected IMediator Mediator =>
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator is unavalible");
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (!result.IsSuccess && result.Code == 404)
+            {
+                return NotFound();
+            }
+
+            if (result.IsSuccess && result.Value != null)
+            {
+                return Ok(result.Value);
+            }
+
+            return BadRequest(result.Error);
+        }
+    }
+}
